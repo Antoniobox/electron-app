c4166406193cf73b51fb7537031f120de81111eb
Antoniobox <antonioboxsanchez@gmail.com>
2024-01-19 09:45:57 +0100

    Add field completing

--- app/views/edit/edit.js
+++ app/views/edit/edit.js
@@ -1,23 +1,18 @@
-async function setDataForPacienteForm() {
+function setDataForPacienteForm() {
     const inputId = document.getElementById('id')
     const inputSip = document.getElementById('sip')
     const inputDni = document.getElementById('dni')
     const inputNombre = document.getElementById('nombre')
     const inputApellido1 = document.getElementById('apellido1')
-    await window.api.onEditPaciente((paciente)=>{
-        console.log("Esto se está ejecutando")
+
+    window.api.onEditPaciente((paciente)=>{
         inputId.setAttribute('value', paciente._id)
         inputSip.setAttribute('value', paciente._sip)
         inputDni.setAttribute('value', paciente._dni)
         inputNombre.setAttribute('value', paciente._nombre)
         inputApellido1.setAttribute('value', paciente._apellido1)
     })
+    
 }
 
-function initSetDataForPacienteForm() {
-    try {
-        setDataForPacienteForm()
-    }catch(error){
-        console.error(error)
-    }
-}
+setDataForPacienteForm()
